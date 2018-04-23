d40572dc83e13b5d8718c8b7db5599aa690779c6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_RowDoubleClick/Products.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+public class Product {
+    public string ProductName { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public double UnitPrice { get; set; }
+    public int Quantity { get; set; }
+}
+
+public class ProductList : ObservableCollection<Product> {
+    public ProductList()
+        : base() {
+        Add(new Product() { ProductName = "Chang", Country = "UK", City = "Cowes", UnitPrice = 19, Quantity = 10 });
+        Add(new Product() { ProductName = "Gravad lax", Country = "Italy", City = "Reggio Emilia", UnitPrice = 12.5, Quantity = 16 });
+        Add(new Product() { ProductName = "Ravioli Angelo", Country = "Brazil", City = "Rio de Janeiro", UnitPrice = 19, Quantity = 12 });
+        Add(new Product() { ProductName = "Tarte au sucre", Country = "Germany", City = "QUICK-Stop", UnitPrice = 22, Quantity = 50 });
+        Add(new Product() { ProductName = "Steeleye Stout", Country = "USA", City = "Reggio Emilia", UnitPrice = 18, Quantity = 20 });
+        Add(new Product() { ProductName = "Pavlova", Country = "Austria", City = "Graz", UnitPrice = 21, Quantity = 52 });
+        Add(new Product() { ProductName = "Longlife Tofu", Country = "USA", City = "Boise", UnitPrice = 7.75, Quantity = 120 });
+        Add(new Product() { ProductName = "Alice Mutton", Country = "Mexico", City = "México D.F.", UnitPrice = 21, Quantity = 15 });
+        Add(new Product() { ProductName = "Alice Mutton", Country = "Canada", City = "Tsawwassen", UnitPrice = 44, Quantity = 16 });
+    }
+}
---
+++ VB/DXGrid_RowDoubleClick/MainWindow.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Input
+Imports DevExpress.Xpf.Grid
+
+Namespace DXGrid_RowDoubleClick
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = New ProductList()
+		End Sub
+
+		Private Sub grid_MouseDoubleClick(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+			Dim rowHandle As Integer = tView.GetRowHandleByMouseEventArgs(e)
+			If rowHandle = GridControl.InvalidRowHandle Then
+				Return
+			End If
+			If grid.IsGroupRowHandle(rowHandle) Then
+				MessageBox.Show("A group row has been double clicked.", "Info")
+			Else
+				MessageBox.Show("A data row has been double clicked.", "Info")
+			End If
+		End Sub
+	End Class
+End Namespace
