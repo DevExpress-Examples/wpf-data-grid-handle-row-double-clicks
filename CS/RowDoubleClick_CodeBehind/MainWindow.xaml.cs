4cfdffad902612d45a803f10abf02c604daa2710
volkov.anton <anton.volkov@devexpress.com>
2021-06-29 13:10:23 +0300

    Update sample - use new build-in event

---
+++ CS/RowDoubleClick_CodeBehind/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using DevExpress.Xpf.Grid;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RowDoubleClick_CodeBehind {
+    public class Product {
+        public string ProductName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public double UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            grid.ItemsSource = new ObservableCollection<Product>(GetProducts());
+        }
+        static IEnumerable<Product> GetProducts() {
+            yield return new Product() { ProductName = "Chang", Country = "UK", City = "Cowes", UnitPrice = 19, Quantity = 10 };
+            yield return new Product() { ProductName = "Gravad lax", Country = "Italy", City = "Reggio Emilia", UnitPrice = 12.5, Quantity = 16 };
+            yield return new Product() { ProductName = "Ravioli Angelo", Country = "Brazil", City = "Rio de Janeiro", UnitPrice = 19, Quantity = 12 };
+            yield return new Product() { ProductName = "Tarte au sucre", Country = "Germany", City = "QUICK-Stop", UnitPrice = 22, Quantity = 50 };
+            yield return new Product() { ProductName = "Steeleye Stout", Country = "USA", City = "Reggio Emilia", UnitPrice = 18, Quantity = 20 };
+            yield return new Product() { ProductName = "Pavlova", Country = "Austria", City = "Graz", UnitPrice = 21, Quantity = 52 };
+            yield return new Product() { ProductName = "Longlife Tofu", Country = "USA", City = "Boise", UnitPrice = 7.75, Quantity = 120 };
+            yield return new Product() { ProductName = "Alice Mutton", Country = "Mexico", City = "México D.F.", UnitPrice = 21, Quantity = 15 };
+            yield return new Product() { ProductName = "Alice Mutton", Country = "Canada", City = "Tsawwassen", UnitPrice = 44, Quantity = 16 };
+        }
+
+        void OnRowDoubleClick(object sender, RowDoubleClickEventArgs e) {
+            int rowHandle = e.HitInfo.RowHandle;
+            if(rowHandle == GridControl.InvalidRowHandle)
+                return;
+            if(grid.IsGroupRowHandle(rowHandle))
+                MessageBox.Show("A group row has been double clicked.", "Info");
+            else
+                MessageBox.Show("A data row has been double clicked.", "Info");
+        }
+    }
+}
